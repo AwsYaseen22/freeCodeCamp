55f6ab5899fd32dfaeb1c3cdbebb61ac08e0e024
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-06-15 22:39:36 +0300

    solve Drop it

--- javascript/IntermediateAlgorithmScripting/answers.js
+++ javascript/IntermediateAlgorithmScripting/answers.js
@@ -280,3 +280,22 @@ function smallestCommons(arr) {
 // console.log(smallestCommons([2, 10]));
 
 // ######################################################
+
+// Drop it
+
+function dropElements(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr.slice(i);
+    }
+  }
+  return [];
+}
+
+// console.log(
+//   dropElements([1, 2, 3, 4], function (n) {
+//     return n >= 3;
+//   })
+// );
+
+// ######################################################
