d5e2722858b38c8408db58bd24165d539d241efb
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-06-21 19:58:20 +0300

    urlshortener project pass all test

--- Backend-Development-APIs/projects/boilerplate-project-urlshortener/index.js
+++ Backend-Development-APIs/projects/boilerplate-project-urlshortener/index.js
@@ -14,6 +14,11 @@ app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
+// instead of the DB collect urls in array
+let urlArray = [];
+// it will hold the short url
+let counter = 1;
+
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
@@ -25,16 +30,43 @@ app.get("/api/hello", function (req, res) {
 
 app.post("/api/shorturl", (req, res) => {
   const url = req.body.url;
-  dns.lookup(url, (err, address, family) => {
-    console.log({ address });
-    console.log({ family });
-    console.log({ err });
+  // let address = url.split(/^https?:\/\//)[1];
+  let address = new URL(url).hostname;
+  console.log(address);
+  dns.lookup(address, (err, address, family) => {
     if (err) {
-      res.status(401).json({
+      res.json({
         error: "invalid url",
       });
+    } else {
+      if (!urlArray.some((e) => e.original_url === url)) {
+        let obj = {
+          original_url: url,
+          short_url: counter,
+        };
+        urlArray.push(obj);
+        counter++;
+        // console.log(obj);
+        // console.log({ original_url: "https://freeCodeCamp.org", short_url: 1 });
+        res.json(obj);
+      } else {
+        let result = urlArray.find((e) => e.original_url === url);
+        res.json(result);
+      }
     }
   });
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  let short = req.params.id;
+  let result = urlArray.find((e) => e.short_url === Number(short));
+  if (result) {
+    res.redirect(result.original_url);
+  } else {
+    res.json({
+      error: "Not found",
+    });
+  }
 });
 
 app.listen(port, function () {
