8776a94ed188c37c4cfbbfc216ae56ccf00ad6db
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-06-22 23:52:49 +0300

    pass 15/16 test almost done just to add optional queries

--- Backend-Development-APIs/projects/boilerplate-project-exercisetracker/index.js
+++ Backend-Development-APIs/projects/boilerplate-project-exercisetracker/index.js
@@ -19,30 +19,40 @@ mongoose.connect(connectionURI, { useNewUrlParser: true }, () => {
 
 let Schema = mongoose.Schema;
 
-// User Schema
-let userSchema = new Schema(
-  {
-    username: { type: String, required: true },
-    log: [],
-  }
-  // {
-  //   toObject: { virtuals: true },
-  // }
-);
-
-userSchema.virtual("count").get(function () {
-  return this.log.length;
+exerciseSchema = new Schema({
+  description: { type: String, required: true },
+  duration: { type: String, required: true },
+  date: Date,
 });
 
+// User Schema
+let userSchema = new Schema({
+  username: { type: String, required: true },
+  log: [exerciseSchema],
+});
+
+let Exercise = new mongoose.model("Exercise", exerciseSchema);
 let User = new mongoose.model("User", userSchema);
+
+// try
+app.use(({ method, url, query, params, body }, res, next) => {
+  console.log(">>> ", method, url);
+  console.log(" QUERY:", query);
+  console.log(" PRAMS:", params);
+  console.log("  BODY:", body);
+  const _json = res.json;
+  res.json = function (data) {
+    console.log(" RESLT:", JSON.stringify(data, null, 2));
+    return _json.call(this, data);
+  };
+  console.log(" ----------------------------");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Create new user
-// You can POST to /api/users with form data username to create a new user.
-// Waiting:The returned response from POST /api/users with form data username will be an object with username and _id properties.
 app.post("/api/users", async (req, res) => {
   const username = req.body.username;
   try {
@@ -52,16 +62,10 @@ app.post("/api/users", async (req, res) => {
       _id: createdUser._id,
     });
   } catch (error) {
-    console.log(error);
     res.status(500).send(error);
   }
 });
 
-/*
-You can make a GET request to /api/users to get a list of all users.
-Waiting:The GET request to /api/users returns an array.
-Waiting:Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
-*/
 app.get("/api/users", async (req, res) => {
   try {
     let users = await User.find().select({
@@ -69,67 +73,60 @@ app.get("/api/users", async (req, res) => {
       username: true,
       __v: true,
     });
-    console.log(users);
     res.json(users);
   } catch (error) {
-    console.log(error);
     res.status(500).send(error);
   }
 });
 
-/*
-You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
-Waiting:The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
-*/
 app.post("/api/users/:_id/exercises", async (req, res) => {
+  // console.log("POST /api/users/:_id/exercises");
   let { description, duration, date } = req.body;
+  duration = Number(duration);
   let id = req.params._id;
-  // console.log({ id });
-  // console.log({ description });
-  // console.log({ duration });
-  // console.log({ date });
-  date =
-    new Date(date).toDateString() !== "Invalid Date" ||
-    new Date().toDateString();
-  let user = await User.findOne({ _id: id });
-  let obj = {
-    username: user.username,
-    description: description,
-    duration: duration,
-    date: date,
-    _id: id,
-  };
+
+  if (new Date(date).getDate() === new Date(date).getDate()) {
+    date = new Date(date).toDateString();
+  } else {
+    date = new Date().toDateString();
+  }
+
   let updateUser = await User.updateOne(
     { _id: id },
     { $push: { log: { description, duration, date } } }
   );
-  let updatedFinal = await User.findOne({ _id: id });
-  console.log(updatedFinal);
-  res.json(obj);
+  let updatedFinal = await User.findOne({ _id: id }).select({
+    username: true,
+    _id: true,
+    log: true,
+  });
+  let temp = { ...updatedFinal._doc };
+  let logdetails = temp.log[temp.log.length - 1];
+  let result = {
+    username: temp.username,
+    description: logdetails.description,
+    duration: logdetails.duration,
+    date: logdetails.date,
+    _id: temp._id,
+  };
+  // updatedFinal.log = updatedFinal.log[updatedFinal.log.length - 1];
+  // updatedFinal.duration = updatedFinal.log[0].duration;
+  // updatedFinal.date = updatedFinal.log[0].date;
+  // updatedFinal.description = updatedFinal.log[0].description;
+  // console.log(result);
+  return res.json(result);
 });
 
-/*
-You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
-Waiting:A request to a user's log GET /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
-Waiting:A GET request to /api/users/:id/logs will return the user object with a log array of all the exercises added.
-Waiting:Each item in the log array that is returned from GET /api/users/:id/logs is an object that should have a description, duration, and date properties.
-Waiting:The description property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.
-Waiting:The duration property of any object in the log array that is returned from GET /api/users/:id/logs should be a number.
-Waiting:The date property of any object in the log array that is returned from GET /api/users/:id/logs should be a string.. Use the dateString format of the Date API.
-*/
 app.get("/api/users/:_id/logs", async (req, res) => {
+  console.log("GET /api/users/:_id/logs");
   let id = req.params._id;
-
   let foundUser = await User.findOne({ _id: id }).select({
     _id: true,
     username: true,
     log: true,
     count: true,
   });
-  foundUser.count = foundUser.count;
-  console.log(foundUser.count);
-  console.log(foundUser.count);
-  console.log(foundUser);
+  foundUser._doc.count = foundUser._doc.log.length;
   res.json(foundUser);
 });
 
