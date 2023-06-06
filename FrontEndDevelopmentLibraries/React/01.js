a94df060a14cf20d4199aa38bdcd33eb327fd44d
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-06-06 15:00:09 +0300

    Use Array.filter() to Dynamically Filter an Array

--- FrontEndDevelopmentLibraries/React/01.js
+++ FrontEndDevelopmentLibraries/React/01.js
@@ -1119,3 +1119,49 @@ function Frameworks () {
         </div>
     );
 };
+
+
+// Use Array.filter() to Dynamically Filter an Array
+class MyComponent extends React.Component {
+    constructor( props ) {
+        super( props );
+        this.state = {
+            users: [
+                {
+                    username: 'Jeff',
+                    online: true
+                },
+                {
+                    username: 'Alan',
+                    online: false
+                },
+                {
+                    username: 'Mary',
+                    online: true
+                },
+                {
+                    username: 'Jim',
+                    online: false
+                },
+                {
+                    username: 'Sara',
+                    online: true
+                },
+                {
+                    username: 'Laura',
+                    online: true
+                }
+            ]
+        };
+    }
+    render () {
+        const usersOnline = this.state.users.filter( u => u.online ); // Change this line
+        const renderOnline = usersOnline.map( ( e, i ) => ( <li key={i + new Date()}>{e.username}</li> ) ); // Change this line
+        return (
+            <div>
+                <h1>Current Online Users:</h1>
+                <ul>{renderOnline}</ul>
+            </div>
+        );
+    }
+}
