6f4715fb2ae3957d965bc44ae796c189fc78a33a
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-06-06 14:23:44 +0300

    Give Sibling Elements a Unique Key Attribute

--- FrontEndDevelopmentLibraries/React/01.js
+++ FrontEndDevelopmentLibraries/React/01.js
@@ -1095,3 +1095,27 @@ class MyToDoList extends React.Component {
         );
     }
 }
+
+
+
+// Give Sibling Elements a Unique Key Attribute
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+];
+
+function Frameworks () {
+    const renderFrameworks = frontEndFrameworks.map( ( e, i ) => <li key={i + new Date()}>{e}</li> ); // Change this line
+    return (
+        <div>
+            <h1>Popular Front End JavaScript Frameworks</h1>
+            <ul>
+                {renderFrameworks}
+            </ul>
+        </div>
+    );
+};
