6902cb208eda3b4929af1c12415a5c7387da8ede
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-06-08 21:10:38 +0300

    Use the Spread Operator on Arrays

--- FrontEndDevelopmentLibraries/redux.js
+++ FrontEndDevelopmentLibraries/redux.js
@@ -345,21 +345,47 @@ const actionCreator = () => action
 
 
 // Never Mutate State
-const ADD_TO_DO = 'ADD_TO_DO';
+// const ADD_TO_DO = 'ADD_TO_DO';
 
-// A list of strings representing tasks to do:
-const todos = [
-    'Go to the store',
-    'Clean the house',
-    'Cook dinner',
-    'Learn to code',
-];
+// // A list of strings representing tasks to do:
+// const todos = [
+//     'Go to the store',
+//     'Clean the house',
+//     'Cook dinner',
+//     'Learn to code',
+// ];
 
-const immutableReducer = ( state = todos, action ) => {
+// const immutableReducer = ( state = todos, action ) => {
+//     switch ( action.type ) {
+//         case ADD_TO_DO:
+//             // Don't mutate state here or the tests will fail
+//             return [...todos, action.todo]
+//         default:
+//             return state;
+//     }
+// };
+
+// const addToDo = ( todo ) => {
+//     return {
+//         type: ADD_TO_DO,
+//         todo
+//     }
+// }
+
+// const store = Redux.createStore( immutableReducer );
+
+
+
+
+
+
+
+// Use the Spread Operator on Arrays
+const immutableReducer = ( state = ['Do not mutate state!'], action ) => {
     switch ( action.type ) {
-        case ADD_TO_DO:
+        case 'ADD_TO_DO':
             // Don't mutate state here or the tests will fail
-            return [...todos, action.todo]
+            return [...state, action.todo]
         default:
             return state;
     }
@@ -367,7 +393,7 @@ const immutableReducer = ( state = todos, action ) => {
 
 const addToDo = ( todo ) => {
     return {
-        type: ADD_TO_DO,
+        type: 'ADD_TO_DO',
         todo
     }
 }
