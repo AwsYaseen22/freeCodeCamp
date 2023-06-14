d56719a2cc9c20773fc54641ef7273b47f8716f9
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-06-14 16:16:19 +0300

    use click style and play sound

--- FrontEndDevelopmentLibraries/projects/drum_machine/drum-machine/src/App.js
+++ FrontEndDevelopmentLibraries/projects/drum_machine/drum-machine/src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
+import { useState } from 'react'
 
 function App () {
   let links = [
-    'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
-    'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
-    'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
-    'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
-    'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
-    'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
-    'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
-    'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
-    'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    { key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
+    { key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
+    { key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
+    { key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
+    { key: 'S', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
+    { key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
+    { key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
+    { key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
+    { key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' }
   ];
+
+
+  const handlPress = ( e, index ) => {
+    console.log( e.target.classList );
+    e.currentTarget.classList.toggle( 'keyPressed' )
+    setTimeout( () => {
+      e.target.classList.toggle( 'keyPressed' )
+    }, 150 );
+    console.log( { index } )
+    new Audio( links[index].link ).play();
+  }
+
   return (
     <div className="App">
       <div className="keys">
-        {links.map( ( d, i ) => <div key={i} className='key'>{i}</div> )}
+        {links.map( ( d, i ) => <div key={i} className='key' onClick={( e ) => handlPress( e, i )} style={{ userSelect: 'none' }}>{d.key}</div> )}
       </div>
       <div className="settings">
         <div className="innerSettings">
