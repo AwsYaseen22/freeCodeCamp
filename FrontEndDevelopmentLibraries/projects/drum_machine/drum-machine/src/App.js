99328b66f2d93ebdd5e8bff9904ea0e2bb670e27
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-06-14 22:39:44 +0300

    pass 3/8 three test

--- FrontEndDevelopmentLibraries/projects/drum_machine/drum-machine/src/App.js
+++ FrontEndDevelopmentLibraries/projects/drum_machine/drum-machine/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App () {
 
   let keys = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
 
   let obj = {
-    q: { key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', selected: false, index: 0 },
-    w: { key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', selected: false, index: 1 },
-    e: { key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', selected: false, index: 2 },
-    a: { key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', selected: false, index: 3 },
-    s: { key: 'S', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', selected: false, index: 4 },
-    d: { key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', selected: false, index: 5 },
-    z: { key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', selected: false, index: 6 },
-    c: { key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', selected: false, index: 8 },
-    x: { key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', selected: false, index: 7 },
+    q: { key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', display: 'Heater 1', selected: false, index: 0 },
+    w: { key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', display: 'Heater 2', selected: false, index: 1 },
+    e: { key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', display: 'Heater 3', selected: false, index: 2 },
+    a: { key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', display: 'Heater 4', selected: false, index: 3 },
+    s: { key: 'S', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', display: 'Clap', selected: false, index: 4 },
+    d: { key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', display: 'Open HH', selected: false, index: 5 },
+    z: { key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', display: 'Kick n Hat', selected: false, index: 6 },
+    c: { key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', display: 'Kick', selected: false, index: 8 },
+    x: { key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', display: 'Closed HH', selected: false, index: 7 },
   }
+
+  const [display, setDisplay] = useState( '...' )
 
   useEffect( () => {
     function handleKeyDown ( e ) {
@@ -39,7 +41,7 @@ function App () {
     setTimeout( () => {
       e.classList.toggle( 'keyPressed' )
     }, 150 );
-
+    setDisplay( item.display )
     new Audio( obj[key].link ).play();
 
   }
@@ -51,8 +53,7 @@ function App () {
       </div>
       <div className="settings">
         <div className="innerSettings">
-          <h1>test</h1>
-          <h1>test</h1>
+          <h2 id='display'>{display}</h2>
         </div>
       </div>
     </div>
