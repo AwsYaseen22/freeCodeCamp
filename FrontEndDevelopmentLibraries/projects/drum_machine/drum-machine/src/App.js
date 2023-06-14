c02094fb3fbc38ecf8155b77b50ee528ea34dca6
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-06-14 21:39:15 +0300

    reshape the login and add keyboard keys

--- FrontEndDevelopmentLibraries/projects/drum_machine/drum-machine/src/App.js
+++ FrontEndDevelopmentLibraries/projects/drum_machine/drum-machine/src/App.js
@@ -1,34 +1,53 @@
 import './App.css';
-import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App () {
-  let links = [
-    { key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
-    { key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
-    { key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
-    { key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
-    { key: 'S', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
-    { key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
-    { key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
-    { key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
-    { key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' }
-  ];
 
+  let keys = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
 
-  const handlPress = ( e, index ) => {
-    console.log( e.target.classList );
-    e.currentTarget.classList.toggle( 'keyPressed' )
+  let obj = {
+    q: { key: 'Q', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', selected: false, index: 0 },
+    w: { key: 'W', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', selected: false, index: 1 },
+    e: { key: 'E', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3', selected: false, index: 2 },
+    a: { key: 'A', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3', selected: false, index: 3 },
+    s: { key: 'S', link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3', selected: false, index: 4 },
+    d: { key: 'D', link: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3', selected: false, index: 5 },
+    z: { key: 'Z', link: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', selected: false, index: 6 },
+    c: { key: 'C', link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3', selected: false, index: 8 },
+    x: { key: 'X', link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', selected: false, index: 7 },
+  }
+
+  useEffect( () => {
+    function handleKeyDown ( e ) {
+      if ( [81, 87, 69, 65, 83, 68, 90, 88, 67].includes( e.keyCode ) ) {
+        handlPress( obj[e.key].key )
+      }
+    }
+    document.addEventListener( 'keydown', handleKeyDown );
+
+    // clean up
+    return function cleanup () {
+      document.removeEventListener( 'keydown', handleKeyDown );
+    }
+  }, [] );
+
+  const handlPress = ( key ) => {
+    key = key.toLowerCase()
+    let item = obj[key]
+    let e = document.getElementById( `key${item.index}` )
+    e.classList.toggle( 'keyPressed' )
     setTimeout( () => {
-      e.target.classList.toggle( 'keyPressed' )
+      e.classList.toggle( 'keyPressed' )
     }, 150 );
-    console.log( { index } )
-    new Audio( links[index].link ).play();
+
+    new Audio( obj[key].link ).play();
+
   }
 
   return (
     <div className="App">
       <div className="keys">
-        {links.map( ( d, i ) => <div key={i} className='key' onClick={( e ) => handlPress( e, i )} style={{ userSelect: 'none' }}>{d.key}</div> )}
+        {keys.map( ( d, i ) => <div key={i} className='key' id={`key${i}`} onClick={( e ) => handlPress( d )} style={{ userSelect: 'none' }}>{d.toUpperCase()}</div> )}
       </div>
       <div className="settings">
         <div className="innerSettings">
